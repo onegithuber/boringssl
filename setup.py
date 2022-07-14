37453dcbeebf2fecc2cd4de42b903413e2531f8b
onegithuber <962182044@qq.com>
2022-07-14 17:21:55 +0800

    build boringssl python module

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class cmake_build_ext(build_ext):
 
 setuptools.setup(
     name='boringssl-bin',
-    version='0.0.1',
+    version='0.0.2',
     description='Build BoringSSL',
     long_description='Build BoringSSL',
     package_dir={"boringssl_bin": "."},
